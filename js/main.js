8acff2d2ac1313bf2e965cff88f85d7079bce694
lyarullina <lyarullina@kazanworkout.ru>
2022-11-18 02:29:16 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -14,31 +14,3 @@ const createPhotos = Array.from({length: SIMILAR_PHOTO_COUNT}, createPhoto);
 
 //createPhotos();
 
-
-// Функция checkStringLength для проверки максимальной длины строки
-function checkStringLength (string, length) {
-  return string.length <= length;
-}
-
-// задание 3
-function getRandomIntInclusive(min, max) {
-  if (min > 0 && !isNaN(max)) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  } else {
-    return NaN;
-  }
-}
-
-//getRandomIntInclusive(1,10);
-
-function getStringLength (string, maxlength) {
-  if (string.length <= maxlength) {
-    return true;
-  }else{
-    return false;
-  }
-}
-
-//getStringLength('Привет',10);
