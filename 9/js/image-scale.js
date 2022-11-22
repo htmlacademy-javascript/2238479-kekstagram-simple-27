4659574065bb2cdf0fcc349400bd83a6247461b3
Keks <keks@htmlacademy.ru>
2022-11-23 01:13:50 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/image-scale.js
@@ -0,0 +1,41 @@
+const scaleSmaller = document.querySelector('.scale__control--smaller');
+const scaleBigger = document.querySelector('.scale__control--bigger');
+const scaleValue = document.querySelector('.scale__control--value');
+const imgPreview = document.querySelector('.img-upload__preview img');
+
+const SCALE_STEP = 25;
+const MIN_SCALE = 25;
+const MAX_SCALE = 100;
+const DEFAULT_SCALE = 100;
+
+const scaleImage = (value = DEFAULT_SCALE) => {
+  imgPreview.style.transform = `scale(${value / 100})`;
+  scaleValue.value = `${value}%`;
+};
+
+const onSmallerClick = () => {
+  const currentValue = parseInt(scaleValue.value, 10);
+  let newValue = currentValue - SCALE_STEP;
+  if (newValue < MIN_SCALE) {
+    newValue = MIN_SCALE;
+  }
+  scaleImage(newValue);
+};
+
+const onBiggerClick = () => {
+  const currentValue = parseInt(scaleValue.value, 10);
+  let newValue = currentValue + SCALE_STEP;
+  if (newValue > MAX_SCALE) {
+    newValue = MAX_SCALE;
+  }
+  scaleImage(newValue);
+};
+
+const resetScale = () => {
+  scaleImage();
+};
+
+scaleSmaller.addEventListener('click', onSmallerClick);
+scaleBigger.addEventListener('click', onBiggerClick);
+
+export {resetScale};
